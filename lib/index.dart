0b8e744be91aa0d1ab2a98b6411a09907b4e578f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-30 23:30:43 +0000

    the beginning

---
+++ lib/index.dart
@@ -0,0 +1,21 @@
+// Export pages
+export '/b_a_r_m_a_n_interfaces/home_page/home_page_widget.dart'
+    show HomePageWidget;
+export '/b_a_r_m_a_n_interfaces/events/events_widget.dart' show EventsWidget;
+export '/b_a_r_m_a_n_interfaces/reservations/reservations_widget.dart'
+    show ReservationsWidget;
+export '/b_a_r_m_a_n_interfaces/profile/profile_widget.dart' show ProfileWidget;
+export '/b_a_r_m_a_n_interfaces/sub_folders/notification_user/notification_user_widget.dart'
+    show NotificationUserWidget;
+export '/b_a_r_m_a_n_interfaces/sub_folders/forgot_password_user/forgot_password_user_widget.dart'
+    show ForgotPasswordUserWidget;
+export '/b_a_r_m_a_n_interfaces/sub_folders/password_management_user/password_management_user_widget.dart'
+    show PasswordManagementUserWidget;
+export '/b_a_r_m_a_n_interfaces/sub_folders/event_details_user/event_details_user_widget.dart'
+    show EventDetailsUserWidget;
+export '/b_a_r_m_a_n_interfaces/sub_folders/bar_details_proba1/bar_details_proba1_widget.dart'
+    show BarDetailsProba1Widget;
+export '/b_a_r_m_a_n_interfaces/sub_folders/images/images_widget.dart'
+    show ImagesWidget;
+export '/reservation_settings/reservation_settings_widget.dart'
+    show ReservationSettingsWidget;
